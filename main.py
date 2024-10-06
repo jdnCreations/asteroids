66cfd53e4647d822ddc8ff6428262fcac22e5f48
Jordan Needham <jdneedham@hotmail.com>
2024-10-06 15:44:58 +0800

    game runs

---
+++ main.py
@@ -0,0 +1,23 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    pygame.mixer.quit()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("ASTEROIDS")
+    print("Starting asteroids!")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return  
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
